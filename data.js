962b325f63059294a26eeeb26dab983e6597acf6
jamie116 <ijeim6961@gmail.com>
2026-02-12 17:37:58 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,133 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-4-",
+      "name": "4층 복도",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.7540807392483853,
+        "pitch": -0.15529628662505246,
+        "fov": 1.3116418928398474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5748142721543168,
+          "pitch": 0.19624851987853376,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.4867820550792352,
+          "pitch": 0.28373098537529096,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "자치활동실",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.1783848616953314,
+        "pitch": 0.12333167423024172,
+        "fov": 1.3116418928398474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2458708426191887,
+          "pitch": 0.17432163472382456,
+          "rotation": 0,
+          "target": "0-4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.036354858495652564,
+          "pitch": 0.04837588147278282,
+          "title": "자치활동실",
+          "text": "아주시네존<div>청운위</div><div>청참위</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "강당",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.011434174374251427,
+        "pitch": 0.040724662152230806,
+        "fov": 1.3116418928398474
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.127490225434469,
+          "pitch": 0.12445375994301244,
+          "rotation": 0,
+          "target": "0-4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.15587327250670135,
+          "pitch": 0.04283438952771945,
+          "title": "강당",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
